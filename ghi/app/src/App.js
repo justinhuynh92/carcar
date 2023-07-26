8511efe316f934dc562cc3f86cf29f59d80d5a53
Jonathan <jonathancornejox@gmail.com>
2023-07-26 13:10:36 -0700

    created getManu getModelsand getSales functions appjs

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -5,10 +5,46 @@ import Nav from './Nav';
 import CustomerForm from './CreateCustomerForm';
 import SalespersonForm from './CreateSalespersonForm';
 
-function App(props) {
-  // if(props.sales === undefined) {
-  //   return null;
-  // }
+function App() {
+
+  const [models, setModels] = useState([]);
+  const [manufacturers, setManufacturers] = useState([]);
+  const [sales, setSales] = useState([]);
+  const [customers, setCustomers] = useState([]);
+
+  async function getManufacturers() {
+    const url = 'http://localhost:8100/api/manufacturers/';
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      setManufacturers(data.manufacturers);
+    } else {
+      console.error('Could not fetch the data')
+    }
+  }
+
+  async function getModels() {
+    const url = 'http://localhost:8100/api/models/';
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      setModels(data.models);
+    } else {
+      console.error('Could not fetch the data');
+    }
+  }
+
+  async function getSales() {
+    const url = 'http://localhost:8090/api/sales/';
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      setSales(data.sales)
+    } else {
+      console.error('Could not fetch the data');
+    }
+  }
+
 
 
   return (
--- ghi/app/src/CreateSaleForm.js
+++ ghi/app/src/CreateSaleForm.js
@@ -1,1 +1,7 @@
 import React, { useState } from "react";
+
+function SaleForm() {
+
+}
+
+export default SaleForm
