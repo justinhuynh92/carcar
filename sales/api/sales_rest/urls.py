c0a248eaa25b8be7d4c307c1d9379712d392df1d
Jonathan <jonathancornejox@gmail.com>
2023-07-27 17:47:32 -0700

    added delete url path in urls.py to delete a sale

--- sales/api/sales_rest/urls.py
+++ sales/api/sales_rest/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import api_salespersons, api_salesperson, api_customers
-from .views import api_customer, api_sales
+from .views import api_customer, api_sales, api_sale
 
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path("salespeople/<int:pk>/", api_salesperson, name="api_salesperson"),
     path("customers/", api_customers, name="api_customers"),
     path("customers/<int:pk>/", api_customer, name="api_customer"),
-    path("sales/", api_sales, name="api_sales")
+    path("sales/", api_sales, name="api_sales"),
+    path("sales/<int:pk>/", api_sale, name="api_sale")
 ]
