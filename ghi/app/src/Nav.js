f7e5342a4027099a537ce7c187f41557671b4e8b
Justin Huynh <justinhuynh639@gmail.com>
2023-08-03 11:10:45 -0700

    added drop down to all services/inventory

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -3,62 +3,6 @@ import { NavLink, Link } from 'react-router-dom';
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">CarCar</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -66,7 +10,7 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <NavLink className="nav-link" to="/salesperson/new">Add a Salesperson</NavLink>
             </li>
             <li className="nav-item">
@@ -95,7 +39,7 @@ function Nav() {
             </li>
             <li className="nav-item">
               <NavLink className="nav-link" to="/saleshistory">Salesperson History</NavLink>
-            </li>
+            </li> */}
           <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
               <ul className="navbar-nav">
                 <li className="nav-item dropdown">
@@ -105,10 +49,37 @@ function Nav() {
                   <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
                     <li><Link className="dropdown-item" to="/automobiles">Automobiles List</Link></li>
                     <li><Link className="dropdown-item" to="/automobiles/new">New Automobile</Link></li>
+                    <li><Link className="dropdown-item" to="/manufacturer/new">Add a Manufacturer</Link></li>
+                    <li><Link className="dropdown-item" to="/manufacturer">Manufacturer List</Link></li>
+                    <li><Link className="dropdown-item" to="/model/new">Add a Vehicle Model</Link></li>
                   </ul>
                 </li>
               </ul>
           </div>
+          <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
+              <ul className="navbar-nav" style={{ color: "black" }}>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Sales</a>
+                  <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                    <li><Link className="dropdown-item" to="/salesperson/new">Add a Salesperson</Link></li>
+                    <li><Link className="dropdown-item" to="/salesperson">Salespeople List</Link></li>
+                    <li><Link className="dropdown-item" to="/salerecord/new">Add a Sale Record</Link></li>
+                    <li><Link className="dropdown-item" to="/saleshistory">Salesperson History</Link></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
+              <ul className="navbar-nav" style={{ color: "black" }}>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Customer</a>
+                  <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                    <li><Link className="dropdown-item" to="/customer/new">Add a Customer</Link></li>
+                    <li><Link className="dropdown-item" to="/customer">Customer List</Link></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
           <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
               <ul className="navbar-nav" style={{ color: "black" }}>
                 <li className="nav-item dropdown">
